70f6321db5f370a9f8c8384d1f37f00a31fe3bff
SohamB4746Y <sohambhattacharya023@gmail.com>
2026-04-16 04:30:07 +0530

    Add Pattern 9 diamond star pattern implementation

---
+++ Pattern9.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int k=5;k>i;k--)
+        {
+            cout<<" ";
+        }
+        for (int j=0;j<2*i+1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i = 0;i<5;i++)
+    {
+        for(int k=0;k<=i;k++)
+        {
+            cout<<" ";
+        }
+        for (int j=10;j>2*i+1;j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
