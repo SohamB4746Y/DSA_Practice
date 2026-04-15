ca92484d9f4e2c38df7609a04c0b2d1a117798aa
SohamB4746Y <sohambhattacharya023@gmail.com>
2026-04-16 04:30:00 +0530

    Add Pattern 8 inverted star pyramid implementation

---
+++ Pattern8.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0;i<5;i++)
+    {
+        for(int k=0;k<=i;k++)
+        {
+            cout<<" ";
+        }
+        for (int j=10;j>2*i+1;j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
